20d90036cdccff5616af5e92aa461ad079db9962
estellel-github <131867732+estellel-github@users.noreply.github.com>
2024-07-09 10:09:16 +0200

    Fix for loop

--- javascript/main.js
+++ javascript/main.js
@@ -3,7 +3,7 @@ const container = document.querySelector("#container");
 let gridLength = 16;
 
 for (let i = 1; i <= gridLength; i++) {
-  for (let j = 1; i <= gridLength; j++) {
+  for (let j = 1; j <= gridLength; j++) {
   const newDiv = document.createElement("div");
   newDiv.classList.add("grid-square");
   container.appendChild(newDiv);
