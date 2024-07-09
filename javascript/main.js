6a9328dcecd40ca6c778a08168eca282168c1568
estellel-github <131867732+estellel-github@users.noreply.github.com>
2024-07-09 11:48:30 +0200

    Start implementing user input for # of rows/cols

--- javascript/main.js
+++ javascript/main.js
@@ -1,14 +1,15 @@
 const container = document.querySelector("#container");
 
-let gridLength = 16;
+let numRows = 16;
+let numCols = 16;
 
-for (let i = 1; i <= gridLength; i++) {
+for (let i = 1; i <= numRows; i++) {
   const newRowDiv = document.createElement("div");
   let rowId = "row" + i;
   newRowDiv.setAttribute("id", rowId);
   newRowDiv.classList.add("row");
   container.appendChild(newRowDiv);
-  for (let j = 1; j <= gridLength; j++) {
+  for (let j = 1; j <= numCols; j++) {
   const newSquareDiv = document.createElement("div");
   let squareId = "square" + i + "-" + j;
   newSquareDiv.setAttribute("id", squareId);
