77668df090cc4f7150be48c61e3d8249c178c289
estellel-github <131867732+estellel-github@users.noreply.github.com>
2024-07-09 10:43:24 +0200

    Fix grid logic & adjust styling -- Separate grid into rows -- Add ids for rows and squares

--- javascript/main.js
+++ javascript/main.js
@@ -3,9 +3,15 @@ const container = document.querySelector("#container");
 let gridLength = 16;
 
 for (let i = 1; i <= gridLength; i++) {
+  const newRowDiv = document.createElement("div");
+  let rowId = "row" + i;
+  newRowDiv.setAttribute("id", rowId);
+  container.appendChild(newRowDiv);
   for (let j = 1; j <= gridLength; j++) {
-  const newDiv = document.createElement("div");
-  newDiv.classList.add("grid-square");
-  container.appendChild(newDiv);
+  const newColDiv = document.createElement("div");
+  let squareId = "square" + i + "-" + j;
+  newColDiv.setAttribute("id", squareId);
+  newColDiv.classList.add("grid-square");
+  newRowDiv.appendChild(newColDiv);
   }
 }
