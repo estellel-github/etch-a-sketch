eb4c70246e8cc7d37aa4780721df60fa729e059b
estellel-github <131867732+estellel-github@users.noreply.github.com>
2024-07-09 12:38:44 +0200

    Finish user input implementation Note: Change back to just length of a square grid (not separate row/col values)

--- javascript/main.js
+++ javascript/main.js
@@ -1,22 +1,37 @@
-const container = document.querySelector("#container");
+const containerEl = document.querySelector("#container");
 
-let numRows = 16;
-let numCols = 16;
-
-for (let i = 1; i <= numRows; i++) {
-  const newRowDiv = document.createElement("div");
-  let rowId = "row" + i;
-  newRowDiv.setAttribute("id", rowId);
-  newRowDiv.classList.add("row");
-  container.appendChild(newRowDiv);
-  for (let j = 1; j <= numCols; j++) {
-  const newSquareDiv = document.createElement("div");
-  let squareId = "square" + i + "-" + j;
-  newSquareDiv.setAttribute("id", squareId);
-  newSquareDiv.classList.add("grid-square");
-  newRowDiv.appendChild(newSquareDiv);
-  newSquareDiv.addEventListener("mouseover", (event) => {
-    newSquareDiv.classList.add("grid-square-colored");
-  });
+const updateGrid = (length) => {
+  containerEl.innerHTML = "";
+  for (let i = 1; i <= length; i++) {
+    const newRowDiv = document.createElement("div");
+    let rowId = "row" + i;
+    newRowDiv.setAttribute("id", rowId);
+    newRowDiv.classList.add("row");
+    containerEl.appendChild(newRowDiv);
+    for (let j = 1; j <= length; j++) {
+      const newSquareDiv = document.createElement("div");
+      let squareId = "square" + i + "-" + j;
+      newSquareDiv.setAttribute("id", squareId);
+      newSquareDiv.classList.add("grid-square");
+      newRowDiv.appendChild(newSquareDiv);
+      newSquareDiv.addEventListener("mouseover", (event) => {
+        newSquareDiv.classList.add("grid-square-colored");
+      });
+    }
   }
-}
+};
+
+updateGrid(16, 16);
+
+const applyUserInput = () => {
+  const inputFormEl = document.querySelector("#input-form");
+  const lengthInputEl = document.querySelector("#length-input");
+  inputFormEl.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const length = lengthInputEl.value || 16;
+    updateGrid(length);
+    lengthInputEl.value = "";
+  });
+};
+
+applyUserInput();
