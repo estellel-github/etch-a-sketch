3fdc406106b26c4956ee9b2bfa8e6a6b3e30b9cd
estellel-github <131867732+estellel-github@users.noreply.github.com>
2024-07-09 10:02:04 +0200

    Start implementing grid -- Create container div -- Add loop for grid (16x16) -- Start styling grid

---
+++ javascript/main.js
@@ -0,0 +1,11 @@
+const container = document.querySelector("#container");
+
+let gridLength = 16;
+
+for (let i = 1; i <= gridLength; i++) {
+  for (let j = 1; i <= gridLength; j++) {
+  const newDiv = document.createElement("div");
+  newDiv.classList.add("grid-square");
+  container.appendChild(newDiv);
+  }
+}
