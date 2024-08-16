3dacd6f45beebfaf64cfdaf135c8ec27cc41ddbe
estellel-github <131867732+estellel-github@users.noreply.github.com>
2024-08-16 16:51:20 +0200

    Clean CSS, fix buttons layout, separate JS colors -- Clean whole CSS (inspect to fix any issues and remove unused classes) -- Fix button layout to fit smaller screen -- Make separate script file for color generation and color codes conversion

---
+++ javascript/colors.js
@@ -0,0 +1,50 @@
+// https://martin.ankerl.com/2009/12/09/how-to-create-random-colors-programmatically/
+
+function hsvToRgb(h, s, v) {
+  let r, g, b;
+  let i = Math.floor(h * 6);
+  let f = h * 6 - i;
+  let p = v * (1 - s);
+  let q = v * (1 - f * s);
+  let t = v * (1 - (1 - f) * s);
+
+  switch (i % 6) {
+    case 0:
+      (r = v), (g = t), (b = p);
+      break;
+    case 1:
+      (r = q), (g = v), (b = p);
+      break;
+    case 2:
+      (r = p), (g = v), (b = t);
+      break;
+    case 3:
+      (r = p), (g = q), (b = v);
+      break;
+    case 4:
+      (r = t), (g = p), (b = v);
+      break;
+    case 5:
+      (r = v), (g = p), (b = q);
+      break;
+  }
+
+  return [Math.floor(r * 255), Math.floor(g * 255), Math.floor(b * 255)];
+}
+
+const generateRandomColors = (numColors) => {
+  const GOLDEN_RATIO = 0.618033988749895;
+  let h = Math.random();
+  const colors = [];
+
+  for (let i = 0; i < numColors; i++) {
+    h += GOLDEN_RATIO;
+    h %= 1;
+    colors.push(hsvToRgb(h, 0.5, 0.95));
+  }
+  return colors;
+};
+
+function rgbToHex(r, g, b) {
+  return `#${((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1)}`;
+}
--- javascript/main.js
+++ javascript/main.js
@@ -9,58 +9,6 @@ let mode = DEFAULT_MODE;
 
 let isMouseDown = false;
 
-// https://martin.ankerl.com/2009/12/09/how-to-create-random-colors-programmatically/
-
-function hsvToRgb(h, s, v) {
-  let r, g, b;
-  let i = Math.floor(h * 6);
-  let f = h * 6 - i;
-  let p = v * (1 - s);
-  let q = v * (1 - f * s);
-  let t = v * (1 - (1 - f) * s);
-
-  switch (i % 6) {
-    case 0:
-      (r = v), (g = t), (b = p);
-      break;
-    case 1:
-      (r = q), (g = v), (b = p);
-      break;
-    case 2:
-      (r = p), (g = v), (b = t);
-      break;
-    case 3:
-      (r = p), (g = q), (b = v);
-      break;
-    case 4:
-      (r = t), (g = p), (b = v);
-      break;
-    case 5:
-      (r = v), (g = p), (b = q);
-      break;
-  }
-
-  return [Math.floor(r * 255), Math.floor(g * 255), Math.floor(b * 255)];
-}
-
-const generateRandomColors = (numColors) => {
-  const GOLDEN_RATIO = 0.618033988749895;
-  let h = Math.random();
-  const colors = [];
-
-  for (let i = 0; i < numColors; i++) {
-    h += GOLDEN_RATIO;
-    h %= 1;
-    colors.push(hsvToRgb(h, 0.5, 0.95));
-  }
-  console.log(colors);
-  return colors;
-};
-
-function rgbToHex(r, g, b) {
-  return `#${((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1)}`;
-}
-
 const containerEl = document.querySelector("#grid");
 
 const grayscaleButtonEl = document.querySelector("#grayscale-button");
@@ -176,15 +124,11 @@ function colorSquare(item, index, colors) {
 }
 function applyOpacity(item) {
   let currentOpacity = parseFloat(item.style.opacity) || 0;
-  console.log(currentOpacity);
   currentOpacity += 0.1;
-  console.log(currentOpacity);
   if (currentOpacity > 1) {
     currentOpacity = 1;
   }
-  console.log(currentOpacity);
   item.style.opacity = currentOpacity;
-  console.log(item.style.opacity);
 }
 
 function showActiveButton(mode) {
